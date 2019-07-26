a3e01fdcea095f40e4ac208740d9a36c00c1eedf
kryan1622 <krystalryan95@hotmail.co.uk>
2019-07-26 09:45:55 +0100

    create user working without hashing password

--- models/item.js
+++ models/item.js
@@ -8,12 +8,11 @@ const userSchema = new Schema ({
     password: {
         type: String,
         required: true,
-        minlength:6,
         maxlength:8
     }
 });
 
 const user = mongoose.model('User', userSchema);
 
-module.exports = contentSchema;
-module.exports = item;
+module.exports = userSchema;
+module.exports = user;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const item = require("../models/item.js")
 
+//@route POST users/create
+//desc creates new user
+//@access public
+
+router.post("/create", (req,res) => {
+    const use = new item({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    use.save().then(() => {
+        res.send('complete');
+    });
+})
 
 
 module.exports = router;
