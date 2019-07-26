aceff2f07b3bcfde14d916dc38230cb0bf8d899c
kryan1622 <krystalryan95@hotmail.co.uk>
2019-07-26 09:34:17 +0100

    server.js added doesn't work currently because users.js not complete

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/content', { useNewUrlParser: true}
+).then(
+    () => {console.log("Connection Success")}, err => {console.log("Connection Failed")}
+);
+
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json());
+
+const users = require("./routes/users.js");
+
+app.use("/users", users);
+
+app.listen(port, () => console.log('server running on port 5000'));
